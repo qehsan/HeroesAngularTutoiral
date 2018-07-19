4b93b4f6528ea6223016d03bcc60f8241879e8d7
Qasim <qasim_ehsan@outlook.com>
2018-07-18 23:07:56 -0400

    angularfolder added

---
+++ Angular/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id:11, name:'Mr. Nice'},
+    {id:12, name:'Salman'},
+    {id:13, name:'Amir'},
+    {id:14, name:'Hamayun'},
+    {id:15, name:'Shahruk'},
+    {id:16, name:'Benidict'},
+    {id:17, name:'John Snow'},
+    {id:18, name:'Harvey Spectar'},
+    {id:19, name:'Michel James Rose'},
+    {id:20, name:'Dr IQ'}
+];
